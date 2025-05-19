3d920c2c6352730257c8882c2b350828676a3331
meepzorp <115584429+meepzorp@users.noreply.github.com>
2025-05-19 13:41:55 -0700

    chore: Remove ignored files from git tracking

--- make-executable.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-chmod +x create-full-project.sh
-chmod +x create-python-files.sh
-chmod +x master-create-project.sh
-
-echo "Scripts are now executable!"
-echo "Run ./master-create-project.sh to create your project"
